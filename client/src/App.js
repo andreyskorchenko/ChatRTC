514bb044414cbf05e4c20d6a23a2c7545e4c2d4c
andreyskorchenko <andreyskorchenko@gmail.com>
2022-01-03 23:50:55 +0300

    feature(app): added router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import Room from '~/pages/room';
+
+import {
+  BrowserRouter as Router,
+  Switch, Route, Redirect
+} from 'react-router-dom';
+
+import Join from '~/pages/Join';
+import Room from '~/pages/Room';
 
 const App = () => {
-  return <Room />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Join} />
+        <Route path='/room' component={Room} />
+        <Redirect to='/' />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
