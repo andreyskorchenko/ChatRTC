7897e00a63a479efdbca7e06a077462d655768b7
andreyskorchenko <andreyskorchenko@gmail.com>
2022-01-06 00:19:23 +0300

    improve(app): changed build config and updated packages

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,80 +1,79 @@
 const path = require("path");
-const webpack = require("webpack");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const isDev = process.env.MODE_BUILD === "development";
+
 module.exports = {
-  devtool: 'source-map',
+  mode: isDev ? "development" : "production",
+  devtool: "source-map",
+  entry: {
+    app: "./src/index.js",
+  },
+  output: {
+    filename: "js/[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
   devServer: {
-    historyApiFallback: true,
-    contentBase: path.resolve(__dirname, "dist"),
     open: false,
     compress: true,
     hot: true,
     port: 8080,
-  },
-  entry: {
-    src: "./src/index.js",
-  },
-  output: {
-    filename: "js/bundle.js",
-    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: "/node_modules/",
-        use: ["babel-loader"],
+        use: [{ loader: "babel-loader" }],
       },
       {
         test: /\.(s)?css$/,
         use: [
-          { loader: MiniCssExtractPlugin.loader, options: { publicPath: "/" } },
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
+          { loader: isDev ? "style-loader" : MiniCssExtractPlugin.loader },
+          { loader: "css-loader" },
+          { loader: "postcss-loader" },
+          { loader: "sass-loader" },
         ],
       },
       {
-        test: /\.jp(e)?g|png|gif$/i,
-        use: [
-          {
-            loader: "file-loader",
-            options: { name: "[name].[ext]", outputPath: "assets/img" },
-          },
-        ],
+        test: /\.svg$/,
+        type: "asset/inline",
       },
       {
-        test: /\.svg$/i,
-        use: [
-          {
-            loader: "file-loader",
-            options: { name: "[name].[ext]", outputPath: "assets/icons" },
-          },
-        ],
+        test: /\.(jp(e)?g|png|gif)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/images/[contenthash][ext]",
+        },
       },
       {
-        test: /\.woff(2)?|ttf|eot|otf$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: { name: "[name].[ext]", outputPath: "assets/fonts" },
-          },
-        ],
+        test: /\.(woff(2)?|ttf|eot|otf)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/fonts/[contenthash][ext]",
+        },
       },
     ],
   },
   resolve: {
     alias: {
-      '~': path.resolve(__dirname, 'src')
-    }
+      "~": path.resolve(__dirname, "src"),
+    },
+    extensions: [".js"],
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HTMLWebpackPlugin({ template: "./src/index.html" }),
-    new MiniCssExtractPlugin({ filename: "css/style.css" }),
-    new CopyWebpackPlugin({ patterns: [{ from: "src/assets", to: "assets" }] }),
-  ]
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[contenthash].css",
+    }),
+  ],
 };
