acb1d5646e02c864f21682403e02fb142c9cf5c3
andreyskorchenko <andreyskorchenko@gmail.com>
2022-01-05 14:36:07 +0300

    improve(index): removed context

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,7 @@ import { render } from 'react-dom';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-import { WSContext } from './context/WSContext';
 import App from './App';
 import './index.scss';
 
-render(
-  <WSContext>
-    <App />
-  </WSContext>,
-  document.getElementById('root')
-);
+render(<App />, document.getElementById('root'));
