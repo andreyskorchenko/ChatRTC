eb4738d0158dc6ed148b387068324e9ecf9cc34c
andreyskorchenko <andreyskorchenko@gmail.com>
2022-01-30 13:56:30 +0300

    feature(reducer): added case for check authorization

--- client/src/store/reducers/authReducer.js
+++ client/src/store/reducers/authReducer.js
@@ -1,8 +1,10 @@
-import { AUTH_SIGNIN } from '../types';
+import { AUTH_CHECK, AUTH_SIGNIN } from '~/store/types';
 
 const initialState = { isAuth: false };
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
+    case AUTH_CHECK:
+      return { ...state, ...action.payload, isAuth: true };
     case AUTH_SIGNIN:
       return { ...state, ...action.payload, isAuth: true };
     default:
