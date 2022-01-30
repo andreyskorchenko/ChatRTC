fd1b7eddd59b73a2a182b46928e7c64f46da1832
andreyskorchenko <andreyskorchenko@gmail.com>
2022-01-30 10:29:29 +0300

    feature(store): added middleware for redux thunk and connecten to redux devtool

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,11 +1,15 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import authReducer from './reducers/authReducer';
 
 const store = createStore(
   combineReducers({
     auth: authReducer
-  })
+  }), compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;
