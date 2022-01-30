9be65ddda8babd126bba0b35d63142081637727b
andreyskorchenko <andreyskorchenko@gmail.com>
2022-01-30 17:46:44 +0300

    fix(signin): change way sign in on async action

--- client/src/pages/Signin/index.js
+++ client/src/pages/Signin/index.js
@@ -1,27 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import fingerprint from '@fingerprintjs/fingerprintjs';
-import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { authSignin } from '~/store/actions/authActions';
 import './style.scss';
 
 const Signin = () => {
+  const dispatch = useDispatch();
   const [ nickname, setNickname ] = useState('');
-  const [ inProcess, setInProcess ] = useState(false);
   const [ isDisabled, setIsDisabled ] = useState(true);
 
   useEffect(() => {
-    setIsDisabled(!(/^[a-z0-9]{1,30}$/i.test(nickname)));
+    setIsDisabled(!(new RegExp('^[a-z0-9]{1,30}$', 'i').test(nickname)));
   }, [nickname]);
 
   const handlerSetNickname = ({ target }) => {
     setNickname(target.value.trim().toLowerCase());
   };
 
-  const handlerSignin = async ({ code, type }) => {
+  const handlerSignin = ({ code, type }) => {
     const allowKeys = ['Enter', 'NumpadEnter', 'click'];
     if (!allowKeys.includes(code || type) || isDisabled) return;
-    const uid = await (await (await fingerprint.load()).get()).visitorId;
-    const { data: response } = await axios.post('http://localhost:3000/auth/signin', { uid, nickname });
-    console.log(response);
+    dispatch(authSignin(nickname));
   };
 
   return (
