942b79acd1156b29d5e649612a200e3c5d34e6b7
andreyskorchenko <andreyskorchenko@gmail.com>
2022-01-31 10:15:55 +0300

    feature(store): connect preloaders reducer

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,11 +1,13 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
-import authReducer from './reducers/authReducer';
+import authReducer from '~/store/reducers/authReducer';
+import preloadersReducer from '~/store/reducers/preloadersReducer';
 
 const store = createStore(
   combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    preloaders: preloadersReducer
   }), compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
