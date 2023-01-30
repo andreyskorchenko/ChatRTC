5d636f8c664014926bc451cd81df838b198111d2
andreyskorchenko <andreyskorchenko@gmail.com>
2023-01-30 17:52:46 +0300

    refactoring

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,23 +1,21 @@
-const path = require("path");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { resolve } = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const isDev = process.env.MODE_BUILD === "development";
+const isDev = process.env.MODE_BUILD === 'development';
 
 module.exports = {
-  mode: isDev ? "development" : "production",
-  devtool: "source-map",
-  entry: {
-    app: "./src/index.js",
-  },
+  mode: isDev ? 'development' : 'production',
+  devtool: 'source-map',
+  entry: resolve(__dirname, 'src/index.js'),
   output: {
-    filename: "js/[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'js/[contenthash].js',
+    path: resolve(__dirname, 'dist'),
     clean: true,
   },
   optimization: {
     splitChunks: {
-      chunks: "all",
+      chunks: 'all',
     },
   },
   devServer: {
@@ -30,50 +28,46 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: "/node_modules/",
-        use: [{ loader: "babel-loader" }],
+        exclude: '/node_modules/',
+        use: [{ loader: 'babel-loader' }],
       },
       {
         test: /\.(s)?css$/,
         use: [
-          { loader: isDev ? "style-loader" : MiniCssExtractPlugin.loader },
-          { loader: "css-loader" },
-          { loader: "postcss-loader" },
-          { loader: "sass-loader" },
+          { loader: isDev ? 'style-loader' : MiniCssExtractPlugin.loader },
+          { loader: 'css-loader' },
+          { loader: 'postcss-loader' },
+          { loader: 'sass-loader' },
         ],
       },
       {
         test: /\.svg$/,
-        type: "asset/inline",
+        type: 'asset/inline',
       },
       {
         test: /\.(jp(e)?g|png|gif)$/,
-        type: "asset/resource",
+        type: 'asset/resource',
         generator: {
-          filename: "assets/images/[contenthash][ext]",
+          filename: 'assets/images/[contenthash][ext]',
         },
       },
       {
         test: /\.(woff(2)?|ttf|eot|otf)$/,
-        type: "asset/resource",
+        type: 'asset/resource',
         generator: {
-          filename: "assets/fonts/[contenthash][ext]",
+          filename: 'assets/fonts/[contenthash][ext]',
         },
       },
     ],
   },
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "src"),
+      '@/': resolve(__dirname, 'src'),
     },
-    extensions: [".js"],
+    extensions: ['.ts', '.tsx', '.js', '.json'],
   },
   plugins: [
-    new HTMLWebpackPlugin({
-      template: "./src/index.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "css/[contenthash].css",
-    }),
+    new HTMLWebpackPlugin({ template: resolve(__dirname, 'public/index.html') }),
+    new MiniCssExtractPlugin({ filename: 'css/[contenthash].css' }),
   ],
 };
