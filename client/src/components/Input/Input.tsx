65ed7bd16673f5ccaa2ad5547da0afa4c3429673
andreyskorchenko <andreyskorchenko@gmail.com>
2023-05-29 15:56:12 +0300

    improve input component

--- client/src/components/Input/Input.tsx
+++ client/src/components/Input/Input.tsx
@@ -1,9 +1,38 @@
-import { ChangeEvent, InputHTMLAttributes } from 'react';
+import { ChangeEvent } from 'react';
+import { SizeTypesKeys, InputTypesKeys } from '@/types';
+import styles from './Input.module.scss';
+import cn from 'classnames';
 
-interface Props extends InputHTMLAttributes<HTMLInputElement> {
+interface Props {
+  autoFocus: boolean;
+  className?: string;
+  maxLength?: number;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  size: SizeTypesKeys;
+  type?: InputTypesKeys;
+  value: string;
 }
 
-export const Input = ({ onChange, ...attrs }: Props) => {
-  return <input {...attrs} onChange={onChange} />;
+export const Input = ({
+  autoFocus,
+  className,
+  maxLength,
+  onChange,
+  placeholder,
+  size = 'm',
+  type = 'text',
+  value,
+}: Props) => {
+  return (
+    <input
+      autoFocus={autoFocus}
+      className={cn(styles.input, styles[`input_size_${size}`], className)}
+      maxLength={maxLength}
+      onChange={onChange}
+      placeholder={placeholder}
+      type={type}
+      value={value}
+    />
+  );
 };
