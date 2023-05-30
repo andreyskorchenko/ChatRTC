00b0f8317c1a5b2f2619b571e154392763a40d04
andreyskorchenko <andreyskorchenko@gmail.com>
2023-05-30 14:28:42 +0300

    refactoring(components): imput

--- client/src/components/Input/Input.tsx
+++ client/src/components/Input/Input.tsx
@@ -1,14 +1,14 @@
 import { InputHTMLAttributes } from 'react';
-import cn from 'classnames';
+import classNames from 'classnames';
 import { SizeTypes } from '@/types';
 import styles from './Input.module.scss';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  className?: string;
   sizeType?: SizeTypes;
+  className?: string;
 }
 
-export const Input = ({ className, sizeType = 'm', onChange, ...attrs }: Props) => {
-  const classes = cn(styles.input, styles[`input_size_${sizeType}`], className);
-  return <input {...attrs} className={classes} onChange={onChange} />;
+export const Input = ({ sizeType = 'm', className, ...attrs }: Props) => {
+  const classesInput = classNames(styles.input, styles[`input_size_${sizeType}`], className);
+  return <input {...attrs} className={classesInput} />;
 };
