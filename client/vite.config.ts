764ad516256f4843ea7dd6f66223162b49bf159a
andreyskorchenko <andreyskorchenko@gmail.com>
2024-11-12 15:09:19 +0300

    improve vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,11 +3,22 @@ import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'node:path';
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 8080,
-    },
-    resolve: {
-        alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
-    },
+	plugins: [react()],
+	server: {
+		port: 8080
+	},
+	resolve: {
+		alias: [{ find: '@', replacement: resolve(__dirname, './src') }]
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: 'modern-compiler',
+				additionalData: `
+    				@use "${resolve(__dirname, './src/app/styles/mixins.scss')}" as *;
+    				@use "${resolve(__dirname, './src/app/styles/variables.scss')}" as *;
+				`
+			}
+		}
+	}
 });
