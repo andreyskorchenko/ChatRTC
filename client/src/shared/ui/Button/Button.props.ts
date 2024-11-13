abdd1cb16787898e805e0e633ac0b39b9d7962d8
andreyskorchenko <andreyskorchenko@gmail.com>
2024-11-13 16:38:27 +0300

    rename types button

--- client/src/shared/ui/Button/Button.props.ts
+++ client/src/shared/ui/Button/Button.props.ts
@@ -1,9 +1,9 @@
 import { PropsWithChildren, ButtonHTMLAttributes } from 'react';
-import { Sizes, Types } from '@/shared/model';
+import { ComponentSizes, ComponentTypes } from '@/shared/model';
 
 export type ButtonProps = PropsWithChildren<
 	Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'type'> & {
-		size?: Sizes;
-		type?: Types;
+		size?: ComponentSizes;
+		type?: ComponentTypes;
 	}
 >;
