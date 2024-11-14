8a5c78f02ca358497c8a7da9fd6b0dcf00700680
andreyskorchenko <andreyskorchenko@gmail.com>
2024-11-14 17:43:53 +0300

    implemented socket provider

---
+++ client/src/app/providers/SocketProvider.tsx
@@ -0,0 +1,13 @@
+import { PropsWithChildren } from 'react';
+import { SocketContext } from '@/shared/context';
+import { io } from 'socket.io-client';
+
+export const SocketProvider = ({ children }: PropsWithChildren) => {
+	const socket = io(import.meta.env.VITE_SOCKET_URL);
+
+	return (
+		<SocketContext.Provider value={socket}>
+			{children}
+		</SocketContext.Provider>
+	);
+};
