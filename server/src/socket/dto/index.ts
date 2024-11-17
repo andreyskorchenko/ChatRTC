20a339061906b78878a23c2a3e909d5973e07cc8
andreyskorchenko <andreyskorchenko@gmail.com>
2024-11-17 19:24:16 +0300

    re-export dto

--- server/src/socket/dto/index.ts
+++ server/src/socket/dto/index.ts
@@ -1,3 +1,6 @@
 export { CreateRoomDto } from './create-room.dto';
 export { ConnectToRoomDto } from './connect-to-room.dto';
 export { DisconnectOfRoomDto } from './disconnect-of-room.dto';
+export { ShareSdpOffer } from './share-sdp-offer.dto';
+export { ShareSdpAnswer } from './share-sdp-answer.dto';
+export { ShareIceCandidate } from './share-ice-candidate.dto';
