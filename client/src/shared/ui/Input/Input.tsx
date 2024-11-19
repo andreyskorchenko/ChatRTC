8ef9ca6c28278007dd5aa2fda848594fd8bbef99
andreyskorchenko <andreyskorchenko@gmail.com>
2024-11-19 15:22:10 +0300

    improve input component

--- client/src/shared/ui/Input/Input.tsx
+++ client/src/shared/ui/Input/Input.tsx
@@ -3,29 +3,23 @@ import cn from 'classnames';
 import { InputProps } from './Input.props';
 import styles from './Input.module.scss';
 
-export const Input = ({ size = 'l', ...props }: InputProps) => {
+export const Input = ({ size = 'l', className, ...props }: InputProps) => {
 	const [isFocus, setIsFocus] = useState(false);
 
 	const inputStyles = cn(
 		styles.input,
 		{
-			[styles.input_focus]: isFocus
-		},
-		{
+			[styles.input_focus]: isFocus,
 			[styles.input_size_l]: size === 'l',
 			[styles.input_size_s]: size === 's',
 			[styles.input_size_m]: size === 'm'
-		}
+		},
+		className
 	);
 
 	return (
 		<div className={inputStyles}>
-			<input
-				{...props}
-				data-testid="input"
-				onFocus={() => setIsFocus(true)}
-				onBlur={() => setIsFocus(false)}
-			/>
+			<input {...props} data-testid="input" onFocus={() => setIsFocus(true)} onBlur={() => setIsFocus(false)} />
 		</div>
 	);
 };
