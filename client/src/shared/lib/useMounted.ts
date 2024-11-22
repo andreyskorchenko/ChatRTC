886e4949e15b13fa934ae83492a3435b07160cd7
andreyskorchenko <andreyskorchenko@gmail.com>
2024-11-22 14:33:13 +0300

    use mounted hook

---
+++ client/src/shared/lib/useMounted.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+export const useMounted = (opened: boolean, unmountedDelayMs: number) => {
+	const [isMounted, setIsMounted] = useState(false);
+
+	useEffect(() => {
+		if (opened && !isMounted) {
+			setIsMounted(true);
+		} else if (!opened && isMounted) {
+			setTimeout(() => {
+				setIsMounted(false);
+			}, unmountedDelayMs);
+		}
+	}, [opened]);
+
+	return [isMounted];
+};
