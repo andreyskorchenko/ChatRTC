684b33b39fc1e0b7d18a379c32d2333ae0b9f569
andreyskorchenko <andreyskorchenko@gmail.com>
2024-11-23 22:35:32 +0300

    improve socket provider

--- client/src/app/providers/SocketProvider.tsx
+++ client/src/app/providers/SocketProvider.tsx
@@ -1,9 +1,36 @@
-import { PropsWithChildren } from 'react';
-import { SocketContext } from '@/shared/context';
+import { PropsWithChildren, useState, useRef } from 'react';
 import { io } from 'socket.io-client';
+import { Event, PubPayload, SubFn, SocketContext } from '@/shared/context';
+// import { useUserContext } from '@/shared/lib';
 
 export const SocketProvider = ({ children }: PropsWithChildren) => {
-	const socket = io(import.meta.env.VITE_SOCKET_URL);
+	// const { username } = useUserContext();
+	const [isConnected, setIsConnected] = useState(false);
+	const socket = useRef(
+		io(import.meta.env.VITE_SOCKET_URL, {
+			auth: {}
+		})
+	);
 
-	return <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>;
+	socket.current.on('connect', () => {
+		setIsConnected(true);
+	});
+
+	socket.current.on('disconnect', () => {
+		setIsConnected(false);
+	});
+
+	const pub = (event: Event, payload?: PubPayload) => {
+		socket.current.emit(event, payload);
+	};
+
+	const sub = <T,>(event: Event, fn: SubFn<T>) => {
+		socket.current.on(event, fn);
+	};
+
+	const unSub = (event: Event, listener?: (...args: unknown[]) => void) => {
+		socket.current.off(event, listener);
+	};
+
+	return <SocketContext.Provider value={{ isConnected, pub, sub, unSub }}>{children}</SocketContext.Provider>;
 };
