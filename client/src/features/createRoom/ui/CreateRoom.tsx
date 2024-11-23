97ef68999f6d030069f6dadb4e2b624f7f8e0d0d
andreyskorchenko <andreyskorchenko@gmail.com>
2024-11-23 22:36:27 +0300

    create room

---
+++ client/src/features/createRoom/ui/CreateRoom.tsx
@@ -0,0 +1,45 @@
+import { useState, useEffect, KeyboardEvent } from 'react';
+import { Input, Button } from '@/shared/ui';
+import { useSocketContext } from '@/shared/lib';
+import styles from './CreateRoom.module.scss';
+
+export const CreateRoom = () => {
+	const { pub, sub, unSub } = useSocketContext();
+	const [roomName, setRoomName] = useState('');
+	const isDisabled = !Boolean(roomName.trim().length);
+
+	useEffect(() => {
+		sub('CREATED_ROOM', () => setRoomName(''));
+
+		return () => {
+			unSub('CREATED_ROOM');
+		};
+	}, []);
+
+	const createHandle = () => {
+		if (isDisabled) return;
+		pub('CREATE_ROOM', { name: roomName.trim() });
+	};
+
+	const createEnterHandle = ({ key }: KeyboardEvent<HTMLInputElement>) => {
+		if (isDisabled || key !== 'Enter') return;
+		pub('CREATE_ROOM', { name: roomName.trim() });
+	};
+
+	return (
+		<div className={styles.create}>
+			<Input
+				className={styles.create__input}
+				placeholder="Room name"
+				maxLength={25}
+				autoFocus
+				onKeyUp={createEnterHandle}
+				onChange={({ target: { value } }) => setRoomName(value)}
+				value={roomName}
+			/>
+			<Button type="primary" className={styles.create__button} disabled={isDisabled} onClick={createHandle}>
+				Create
+			</Button>
+		</div>
+	);
+};
