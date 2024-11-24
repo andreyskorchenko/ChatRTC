5017db858193b1657ef4abd113d463ed701425e7
andreyskorchenko <andreyskorchenko@gmail.com>
2024-11-24 13:59:42 +0300

    re-export context

--- client/src/shared/context/index.ts
+++ client/src/shared/context/index.ts
@@ -1,1 +1,2 @@
-export { SocketContext } from './SocketContext';
+export { UserContext } from './UserContext';
+export { type Event, type PubPayload, type SubFn, type UnSubFn, SocketContext } from './SocketContext';
