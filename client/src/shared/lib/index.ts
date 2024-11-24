fb9d5165150c1bce166ea418e0d8c8805a99e2dc
andreyskorchenko <andreyskorchenko@gmail.com>
2024-11-24 14:01:00 +0300

    re-export lib

--- client/src/shared/lib/index.ts
+++ client/src/shared/lib/index.ts
@@ -1,2 +1,5 @@
+export { useUserContext } from './useUserContext';
 export { useSocketContext } from './useSocketContext';
 export { useAbortController } from './useAbortController';
+export { useLocalStorage } from './useLocalStorage';
+export { useMounted } from './useMounted';
