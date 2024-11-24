1b3430a9281d3448e7b82de04c1f767e84798e72
andreyskorchenko <andreyskorchenko@gmail.com>
2024-11-24 14:38:16 +0300

    improve send message

--- client/src/features/sendMessage/ui/SendMessage.tsx
+++ client/src/features/sendMessage/ui/SendMessage.tsx
@@ -1,28 +1,25 @@
-import { useState, ChangeEvent, KeyboardEvent } from 'react';
+import { useState, KeyboardEvent } from 'react';
 import { Input } from '@/shared/ui';
-import { messageApi } from '../api';
+import { useSocketContext } from '@/shared/lib';
 import { SendMessageProps } from './SendMessage.props';
 import styles from './SendMessage.module.scss';
 
 export const SendMessage = ({ roomId }: SendMessageProps) => {
-	const send = messageApi(roomId);
 	const [message, setMessage] = useState('');
-
-	const changeMessage = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
-		setMessage(value);
-	};
+	const { pub } = useSocketContext();
 
 	const enterMessage = ({ key }: KeyboardEvent<HTMLInputElement>) => {
 		if (key !== 'Enter') return;
-		send(message);
+		pub('SEND_MESSAGE', { roomId, message });
 		setMessage('');
 	};
 
 	return (
 		<Input
 			className={styles['send-message']}
+			autoFocus
 			onKeyUp={enterMessage}
-			onChange={changeMessage}
+			onChange={({ target: { value } }) => setMessage(value)}
 			value={message}
 			placeholder="Typing message and press ENTER"
 		/>
