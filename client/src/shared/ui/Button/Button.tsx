fdd3dd4b95533d44e076b251bd610d960eef185f
andreyskorchenko <andreyskorchenko@gmail.com>
2024-11-24 14:04:11 +0300

    refactoring

--- client/src/shared/ui/Button/Button.tsx
+++ client/src/shared/ui/Button/Button.tsx
@@ -2,20 +2,16 @@ import cn from 'classnames';
 import { ButtonProps } from './Button.props';
 import styles from './Button.module.scss';
 
-export const Button = ({
-	size = 'l',
-	type,
-	children,
-	className,
-	...props
-}: ButtonProps) => {
+export const Button = ({ size = 'l', type, children, className, ...props }: ButtonProps) => {
 	const buttonStyles = cn(
 		styles.button,
 		{
 			[styles.button_size_l]: size === 'l',
 			[styles.button_size_s]: size === 's',
 			[styles.button_size_m]: size === 'm',
-			[styles.button_type_primary]: type === 'primary'
+			[styles.button_type_primary]: type === 'primary',
+			[styles.button_type_outline]: type === 'outline',
+			[styles.button_type_danger]: type === 'danger'
 		},
 		className
 	);
