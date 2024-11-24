a44849684acfd6fbac202b5c05677fcddff2a543
andreyskorchenko <andreyskorchenko@gmail.com>
2024-11-24 13:59:25 +0300

    socket context refactoring

--- client/src/shared/context/SocketContext.tsx
+++ client/src/shared/context/SocketContext.tsx
@@ -1,4 +1,16 @@
 import { createContext } from 'react';
-import { Socket } from 'socket.io-client';
+import { SocketEvents } from '@/shared/model';
 
-export const SocketContext = createContext<Socket | null>(null);
+export type Event = keyof typeof SocketEvents;
+export type PubPayload = Record<string, unknown>;
+export type SubFn<T> = (data: T) => void;
+export type UnSubFn = (...args: unknown[]) => void;
+
+type SocketContext = {
+	isConnected: boolean;
+	pub: (event: Event, payload?: PubPayload) => void;
+	sub: <SubData>(event: Event, fn: SubFn<SubData>) => void;
+	unSub: (event: Event, listener?: UnSubFn) => void;
+};
+
+export const SocketContext = createContext<SocketContext>({} as SocketContext);
