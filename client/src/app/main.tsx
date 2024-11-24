cc17e0ba975dde752e1861a17a901984aba9c7c6
andreyskorchenko <andreyskorchenko@gmail.com>
2024-11-24 14:44:45 +0300

    improve

--- client/src/app/main.tsx
+++ client/src/app/main.tsx
@@ -1,14 +1,16 @@
-// import { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { SocketProvider } from '@/app/providers';
+import { UserProvider, SocketProvider } from '@/app/providers';
 import { router } from '@/app/router';
 import '@/app/styles/main.css';
 
 createRoot(document.getElementById('root')!).render(
-	// <StrictMode>
-	<SocketProvider>
-		<RouterProvider router={router} future={{ v7_startTransition: true }} />
-	</SocketProvider>
-	// </StrictMode>
+	<StrictMode>
+		<UserProvider>
+			<SocketProvider>
+				<RouterProvider router={router} future={{ v7_startTransition: true }} />
+			</SocketProvider>
+		</UserProvider>
+	</StrictMode>
 );
