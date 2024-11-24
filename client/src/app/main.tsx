b81ae8df1280dfa52b588c7699ab5fd02711493e
andreyskorchenko <andreyskorchenko@gmail.com>
2024-11-24 14:54:15 +0300

    remove StrictMode

--- client/src/app/main.tsx
+++ client/src/app/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { UserProvider, SocketProvider } from '@/app/providers';
@@ -6,11 +5,9 @@ import { router } from '@/app/router';
 import '@/app/styles/main.css';
 
 createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<UserProvider>
-			<SocketProvider>
-				<RouterProvider router={router} future={{ v7_startTransition: true }} />
-			</SocketProvider>
-		</UserProvider>
-	</StrictMode>
+	<UserProvider>
+		<SocketProvider>
+			<RouterProvider router={router} future={{ v7_startTransition: true }} />
+		</SocketProvider>
+	</UserProvider>
 );
