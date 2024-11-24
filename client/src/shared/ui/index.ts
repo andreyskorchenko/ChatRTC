45a09ae588ef0c3e62ab514e6f2602fa3696ae4f
andreyskorchenko <andreyskorchenko@gmail.com>
2024-11-24 14:20:26 +0300

    re-export components

--- client/src/shared/ui/index.ts
+++ client/src/shared/ui/index.ts
@@ -1,2 +1,6 @@
+export { Icon } from './Icon';
 export { Input } from './Input';
 export { Button } from './Button';
+export { Overlay } from './Overlay';
+export { Portal } from './Portal';
+export { Popup } from './Popup';
