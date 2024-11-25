423220ae84238f2e2f15a2b42f74c7d6565e4578
andreyskorchenko <andreyskorchenko@gmail.com>
2024-11-25 18:07:15 +0300

    fix ws exceptions filter

--- server/src/socket/filters/ws-exceptions.filter.ts
+++ server/src/socket/filters/ws-exceptions.filter.ts
@@ -1,24 +1,25 @@
 import { Catch, BadRequestException, ArgumentsHost } from '@nestjs/common';
-import { BaseWsExceptionFilter } from '@nestjs/websockets';
+import { BaseWsExceptionFilter, WsException } from '@nestjs/websockets';
 import { SocketEvents } from '@/socket/types';
 import { Socket } from 'socket.io';
 
 @Catch()
 export class WsExceptionsFilter extends BaseWsExceptionFilter {
-	catch(exception: BadRequestException, host: ArgumentsHost) {
-		const response = exception.getResponse();
+	catch(exception: unknown, host: ArgumentsHost) {
 		const ws = host.switchToWs();
 		const client = ws.getClient<Socket>();
 
-		if (
-			typeof response === 'object' &&
-			typeof response !== 'string' &&
-			typeof response !== null &&
-			'message' in response &&
-			Array.isArray(response.message)
-		) {
-			client.emit(SocketEvents.ERROR, { message: response.message });
-		} else {
+		if (exception instanceof BadRequestException) {
+			const response = exception.getResponse();
+
+			if (typeof response !== 'string' && 'message' in response && Array.isArray(response.message)) {
+				return client.emit(SocketEvents.ERROR, { message: response.message });
+			}
+
+			client.emit(SocketEvents.ERROR, { message: ['Bad request'] });
+		}
+
+		if (exception instanceof WsException) {
 			client.emit(SocketEvents.ERROR, { message: ['Bad request'] });
 		}
 	}
