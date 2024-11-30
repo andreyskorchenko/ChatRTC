9f2f8129121c8f8f29ff57870a2cb4ef6a7c7da6
andreyskorchenko <andreyskorchenko@gmail.com>
2024-11-30 12:57:58 +0300

    CI/CD test

--- frontend/src/widgets/rooms/ui/RoomsEmpty.tsx
+++ frontend/src/widgets/rooms/ui/RoomsEmpty.tsx
@@ -5,7 +5,7 @@ export const RoomsEmpty = () => {
 		<div className={styles['rooms-empty']}>
 			<p>
 				There are no rooms to connect to.
-				<br /> You can create your first room using the <span>create room</span> button
+				<br /> You can create your first room using the <span>CREATE ROOM</span> button
 			</p>
 		</div>
 	);
